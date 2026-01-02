311f363d8af0311354fa0e80d0473e086b986fca
Prayash Jena <prayash1305@gmail.com>
2026-01-02 10:51:04 +0530

    First Commit

---
+++ RNW_PATT.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	clrscr();
+	printf("-------\n");
+	printf("|      |\n");
+	printf("|R     |\n");
+	printf("|N     |\n");
+	printf("|W     |\n");
+	printf("|      |\n");
+	printf("-------\n");
+	getch();
+	return 0;
+}
---
+++ TRIANGLE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	float b = 10;
+	float h = 6;
+	float area;
+	clrscr();
+
+	area  = 0.5 * b * h;
+
+	printf("Area of a Triangle = %.2f",area);
+	getch();
+
+	return 0;
+}
