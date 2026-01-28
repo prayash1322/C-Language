e3d14df33c6f70ed84e0a20e0bece432fbab51ec
Prayash Jena <prayash1305@gmail.com>
2026-01-28 10:33:16 +0530

    Little updates

--- Rnw practice/Rnw-pro/Array/Project-Assorment/simple-Project-28-jan/main.c
+++ Rnw practice/Rnw-pro/Array/Project-Assorment/simple-Project-28-jan/main.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     char name[50];
     int roll, sum;
     float per;
     int m1, m2, m3;
+    printf("Welcome to Student Marks Calculator\n");
     printf("Enter your name:- ");
     scanf("%s", name);
 
@@ -17,28 +19,31 @@ int main(){
     scanf("%d", &m2);
     printf("Enter mark 3:- ");
     scanf("%d", &m3);
-    
+
     sum = m1 + m2 + m3;
-    
-    if(sum <= 300){
+
+    if (sum <= 300)
+    {
         per = sum / 3.0;
-        
+
         printf("\n--- Summary ---\n");
         printf("Name: %s\n", name);
         printf("Roll No: %d\n", roll);
         printf("Total Marks: %d\n", sum);
         printf("Percentage: %.2f%%\n", per);
         printf("Result: %s\n", per >= 40 ? "Pass" : "Fail");
-        
-        if(per >= 75)
+
+        if (per >= 75)
             printf("Grade: A\n");
-        else if(per >= 60)
+        else if (per >= 60)
             printf("Grade: B\n");
-        else if(per >= 40)
+        else if (per >= 40)
             printf("Grade: C\n");
         else
             printf("Grade: F\n");
-    } else {
+    }
+    else
+    {
         printf("\nInvalid marks detected! Total marks exceed 300.\n");
     }
     return 0;
